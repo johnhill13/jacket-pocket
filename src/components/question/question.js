d6112909400e26a288bd357be42ce964741f53ce
John H <johnnyhill13@gmail.com>
2019-09-06 14:22:08 -0700

    testing how to orchestrate questions entrance

---
+++ src/components/question/question.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+const Question = () => {
+    return(
+        <>
+            <Header as='h1' textAlign='centered'> here is to see if question component is rendering </Header>
+        </>
+    )
+}
+
+export default Question;
