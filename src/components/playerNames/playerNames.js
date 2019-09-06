ee2af8439b6df0e11d4a45af443d02235b269f43
John H <johnnyhill13@gmail.com>
2019-09-06 16:39:05 -0700

    created 4 playerNames squares not aligned

--- src/components/playerNames/playerNames.js
+++ src/components/playerNames/playerNames.js
@@ -1,12 +1,58 @@
-import React from 'react';
-import {Header} from 'semantic-ui-react';
 
-const playerNames = () => {
-    return(
-        <>
-            <Header as='h1' textAlign='center'>this is the playerNames component and I am working </Header>
-        </>
-    )
+import React, { Component } from 'react';
+import { Container, Segment } from 'semantic-ui-react';
+import posed from 'react-pose';
+import '../question/question';
+import './playerNames.css';
+
+const Box = posed.div({
+    visible: { opacity: 1},
+    hidden: { opacity: 0}
+}); 
+
+class playerNames extends Component { 
+    state = { isVisible: true };
+
+    componentDidMount() {
+        setInterval(() => {
+            this.setState(prevState => { return {isVisible: !prevState.isVisible }});
+        }, 500);
+    }
+
+    render() {
+
+        return(
+            <>
+            <Container Grid>
+                <Segment className="questions-container" raised>
+                    <Box
+                        className="box item"
+                        pose={this.state.isVisible ? 'visible' : 'hidden'} 
+                    />
+                    <Box 
+                        className="box item"
+                        pose={this.state.isVisible ? 'visible' : 'hidden'} 
+                    />
+                    <Box 
+                        className="box item"
+                        pose={this.state.isVisible ? 'visible' : 'hidden'} 
+                    />
+                    <Box 
+                        className="box item"
+                        pose={this.state.isVisible ? 'visible' : 'hidden'} 
+                    />
+                </Segment>
+            </Container>
+            </>
+        );
+    }
 }
 
 export default playerNames;
+
+
+
+
+
+
+
