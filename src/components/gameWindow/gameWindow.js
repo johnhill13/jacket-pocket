a72ceb8026c54b67ab8874e5b60df83bd1289871
John H <johnnyhill13@gmail.com>
2019-09-06 15:06:02 -0700

    demo of sideback slides in successfully

--- src/components/gameWindow/gameWindow.js
+++ src/components/gameWindow/gameWindow.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 import { Container, Segment } from 'semantic-ui-react';
 import posed from 'react-pose';
-
+import Question from '../question/question'
+import '../question/question';
 import './gameWindow.css';
 
 
@@ -33,6 +34,7 @@ class gameWindow extends Component {
                             className="box"
                             pose={this.state.isVisible ? 'visible' : 'hidden'} 
                             />
+                    <Question />
                     </Segment>
                 </Container>    
             </>
@@ -48,21 +50,3 @@ export default gameWindow;
 
 
 
-// const ContainerExampleContainer = () => (
-//   <Container>
-//     <p>
-    //   Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo
-    //   ligula eget dolor. Aenean massa strong. Cum sociis natoque penatibus et
-    //   magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis,
-    //   ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa
-    //   quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget,
-    //   arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo.
-    //   Nullam dictum felis eu pede link mollis pretium. Integer tincidunt. Cras
-    //   dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus.
-    //   Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim.
-    //   Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus
-    //   viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet.
-    //   Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi.
-//     </p>
-//   </Container>
-// )
--- src/components/question/question.js
+++ src/components/question/question.js
@@ -1,12 +1,43 @@
 import React from 'react';
-import {Header} from 'semantic-ui-react';
+import ReactDOM from 'react-dom';
+import posed from 'react-pose';
+import './question.css';
 
-const Question = () => {
-    return(
-        <>
-            <Header as='h1' textAlign='centered'> here is to see if question component is rendering </Header>
-        </>
-    )
+const Sidebar = posed.ul({
+  open: {
+    x: '0%',
+    delayChildren: 200,
+    staggerChildren: 50
+  },
+  closed: { x: '-100%', delay: 300 }
+});
+
+const Item = posed.li({
+  open: { y: 0, opacity: 1 },
+  closed: { y: 20, opacity: 0 }
+});
+
+class Question extends React.PureComponent {
+  state = { isOpen: false };
+
+  componentDidMount() {
+    setTimeout(this.toggle, 1000);
+  }
+
+  toggle = () => this.setState({ isOpen: !this.state.isOpen });
+
+  render() {
+    const { isOpen } = this.state;
+
+    return (
+      <Sidebar className="sidebar" pose={isOpen ? 'open' : 'closed'}>
+        <Item className="item" />
+        <Item className="item" />
+        <Item className="item" />
+        <Item className="item" />
+      </Sidebar>
+    );
+  }
 }
 
 export default Question;
