0cb0d2d70d716e150172a04fe710ac8db702bf12
John H <johnnyhill13@gmail.com>
2019-09-06 11:29:09 -0700

    react beginging gameWindow

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import GameWindow from './components/gameWindow/gameWindow';
+import PlayerNames from './components/playerNames/playerNames';
+
+// import SomeClassName from './components/SomeClassName';
+
+class anotherClassName extends Component {
+  render() {
+    return (
+      <>
+        <GameWindow />
+        <PlayerNames />
+      </>
+  )} 
+}
+
+export default anotherClassName;
---
+++ src/components/playerNames/playerNames.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Header} from 'semantic-ui-react';
+
+const playerNames = () => {
+    return(
+        <>
+            <Header as='h1' textAlign='centered'>this is the playerNames component and I am working </Header>
+        </>
+    )
+}
+
+export default playerNames;
