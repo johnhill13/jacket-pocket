397d687f66456084802d34a7477df69fa634ba52
John H <johnnyhill13@gmail.com>
2019-09-13 12:45:59 -0700

    edit constant

--- src/components/constants.js
+++ src/components/constants.js
@@ -1,3 +1,3 @@
-export const API_URL ='https://jacket-pocket-backend.herokuapp.com/api/v1';
+export const API_URL ='https://jacket-pocket-backend.herokuapp.com/';
 
 // export const API_URL ='http://localhost:4001/api/v1';
